9339fb92b4755473f23d6e29c52bfc3b6ace971d
Anamul Haque <anamulhaque9901@gmail.com>
2024-05-18 13:19:47 +0600

    express route

--- project-1_expressJs/dist/app/app.js
+++ project-1_expressJs/dist/app/app.js
@@ -9,10 +9,45 @@ const port = 3000;
 //parsers
 app.use(express_1.default.json()); // api e json data pathale eita use korte hobe. na hole data asbe nah
 app.use(express_1.default.text()); // api e text data pathale eita use korte hobe. na hole data asbe nah
-app.get("/", (req, res) => {
+//router
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+userRouter.post("/create-user", (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "User is created successfully",
+        data: user,
+    });
+});
+courseRouter.post("/create-course", (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: "Course is created successfully",
+        data: course,
+    });
+});
+//middelwar
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname, req.body);
+    next(); // next use na korle porer function e jabe nah.
+};
+app.get("/", logger, (req, res) => {
+    //api hit: http://localhost:3000?email=anamul@gmail.com&name=anamul
+    console.log(req.query); //  api te ja query hesebe debo ta dekhabe({ email: 'anamul@gmail.com', name: 'anamul' })
     res.send("Hello World!!!!!!");
 });
-app.post("/", (req, res) => {
+app.get("/:userId/:subId", (req, res) => {
+    //api hit: http://localhost:3000/1/2
+    console.log(req.params); //  userId r subId dekhabe({ userId: '1', subId: '2' })
+    res.send("Hello World!!!!!!");
+});
+app.post("/", logger, (req, res) => {
     console.log(req.body);
     // res.send("Got data");
     res.json({
--- project-1_expressJs/src/app/app.ts
+++ project-1_expressJs/src/app/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 const app = express();
 const port = 3000;
 
@@ -6,11 +6,53 @@ const port = 3000;
 app.use(express.json()); // api e json data pathale eita use korte hobe. na hole data asbe nah
 app.use(express.text()); // api e text data pathale eita use korte hobe. na hole data asbe nah
 
-app.get("/", (req: Request, res: Response) => {
+//router
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+
+userRouter.post("/create-user", (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+
+  res.json({
+    success: true,
+    message: "User is created successfully",
+    data: user,
+  });
+});
+
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
+
+  res.json({
+    success: true,
+    message: "Course is created successfully",
+    data: course,
+  });
+});
+
+//middelwar
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.hostname, req.body);
+  next(); // next use na korle porer function e jabe nah.
+};
+
+app.get("/", logger, (req: Request, res: Response) => {
+  //api hit: http://localhost:3000?email=anamul@gmail.com&name=anamul
+  console.log(req.query); //  api te ja query hesebe debo ta dekhabe({ email: 'anamul@gmail.com', name: 'anamul' })
+  res.send("Hello World!!!!!!");
+});
+app.get("/:userId/:subId", (req: Request, res: Response) => {
+  //api hit: http://localhost:3000/1/2
+  console.log(req.params); //  userId r subId dekhabe({ userId: '1', subId: '2' })
   res.send("Hello World!!!!!!");
 });
 
-app.post("/", (req: Request, res: Response) => {
+app.post("/", logger, (req: Request, res: Response) => {
   console.log(req.body);
   // res.send("Got data");
   res.json({
